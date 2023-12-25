ad8f85cb5c7beab8fc240792275a6a72d10c38e2
Fabio Polimeni <fabio@ambient.run>
2023-12-25 10:50:52 +0100

    Simple I/O loop

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,45 @@
+use std::io::{self, Write};
+
+extern crate crossterm;
+extern crate rustyline;
+
+use crossterm::{
+    execute,
+    style::{self, Stylize},
+    cursor,
+    terminal,
+};
+
 fn main() {
-    println!("Hello, world!");
+    // Initialize term instance
+    let mut stdout = io::stdout();
+    
+    // Create a new 'readline' instance
+    let mut rl = rustyline::DefaultEditor::new().unwrap();
+    execute!(stdout, terminal::Clear(terminal::ClearType::All)).unwrap();
+    writeln!(stdout, "{}{}", style::Attribute::Bold, "Welcome to Rustyline!".green()).unwrap();
+    execute!(stdout, style::SetAttribute(style::Attribute::Reset)).unwrap();
+
+    loop {
+
+        let readline = rl.readline(": ");
+        
+        match readline {
+            Ok(input) => {
+                if input == "/exit" || input == "/quit" {
+                    break;
+                } else {
+                    write!(stdout, "").unwrap();
+                    writeln!(stdout, "{}: {}", "󱚠", "Thinking...".italic()).unwrap();
+                }
+            }
+            Err(_) => {
+                writeln!(stdout, "").unwrap();
+                writeln!(stdout, "{}", "[Error]: Invalid Input.".red().italic()).unwrap();
+                break;
+            }
+        }
+    }
+
+    execute!(stdout, terminal::Clear(terminal::ClearType::All), cursor::MoveTo(0,0), cursor::Show).unwrap();
 }
